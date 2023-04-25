cef8ba5d3161aeb7cbbf4e4e3f7eca67cff56a99
Paul <greatdogsite@gmail.com>
2023-04-25 14:44:12 -0500

    Completed the Super Bonus.

---
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/TechJobsController.java
@@ -0,0 +1,29 @@
+package org.launchcode.techjobs.mvc.controllers;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TechJobsController {
+
+    static Map<String,String> actionChoices = new HashMap<>();
+    //--done TODO Super Bonus, moved columnChoices from ListController
+    static HashMap<String, String> columnChoices = new HashMap<>();
+
+    public TechJobsController(){
+        actionChoices.put("search", "Search");
+        actionChoices.put("list", "List");
+
+        columnChoices.put("all", "All");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("location", "Location");
+        columnChoices.put("positionType", "Position Type");
+        columnChoices.put("coreCompetency", "Skill");
+    }
+
+    @ModelAttribute("actions")
+    public static Map getActionChoices(){
+        return actionChoices;
+    }
+}
